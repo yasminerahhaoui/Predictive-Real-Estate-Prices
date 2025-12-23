39cf01980402220898c49165e08a193c8c497dcc
yasminerahhaoui <153822182+yasminerahhaoui@users.noreply.github.com>
2025-12-23 17:49:06 +0100

    Refactor tests to use UserInput class

--- test_app.py
+++ test_app.py
@@ -1,11 +1,40 @@
-from app.model import predict_price
+from app.main import predict_price, UserInput
 
 def test_predict_price_type():
-    # Vérifie que la fonction retourne un nombre
-    result = predict_price({'surface': 50, 'chambres': 2})
-    assert isinstance(result, (int, float))
+    user = UserInput(
+        surface=50,
+        nombre_de_chambres=2,
+        nombre_de_salles_de_bain=1,
+        etage=2,
+        terrasse=0,
+        garage=1,
+        ascenseur=1,
+        piscine=0,
+        securite=1,
+        type_bien="appartement",
+        ville="Casablanca",
+        quartier="Maarif"
+    )
+
+    result = predict_price(user)
+    assert isinstance(result["predicted_price_MAD"], float)
+
 
 def test_predict_price_positive():
-    # Vérifie que le prix prédit n'est pas négatif
-    result = predict_price({'surface': 50, 'chambres': 2})
-    assert result >= 0
+    user = UserInput(
+        surface=60,
+        nombre_de_chambres=3,
+        nombre_de_salles_de_bain=2,
+        etage=3,
+        terrasse=1,
+        garage=1,
+        ascenseur=1,
+        piscine=0,
+        securite=1,
+        type_bien="appartement",
+        ville="Casablanca",
+        quartier="Gauthier"
+    )
+
+    result = predict_price(user)
+    assert result["predicted_price_MAD"] >= 0
